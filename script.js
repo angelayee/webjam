17457b9170cfbfaae68b53a0ffb2a24b28647f5b
angelayee <150845290+angelayee@users.noreply.github.com>
2023-11-15 16:00:32 -0800

    sort button

--- script.js
+++ script.js
@@ -3,3 +3,33 @@ document.getElementById('searchButton').addEventListener('click', function() {
     // Here you can add code to handle the search
     alert("Search for: " + searchQuery);
 });
+document.getElementById('searchButton').addEventListener('click', function() {
+    var searchQuery = document.getElementById('searchInput').value;
+    // Here you can add code to handle the search
+    alert("Search for: " + searchQuery);
+});
+
+function sortProducts() {
+    var container = document.getElementById("products");
+    var products = container.getElementsByClassName("product");
+    var sortOption = document.getElementById("sort").value;
+
+    var sortedProducts = Array.from(products).sort(function (a, b) {
+        var nameA = a.querySelector("h3").innerText;
+        var nameB = b.querySelector("h3").innerText;
+
+        if (sortOption === "asc") {
+            return nameA.localeCompare(nameB);
+        } else {
+            return nameB.localeCompare(nameA);
+        }
+    });
+
+    // Clear existing content
+    container.innerHTML = "";
+
+    // Append sorted products
+    sortedProducts.forEach(function (product) {
+        container.appendChild(product);
+    });
+}
